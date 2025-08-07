b1f627581351bc346b3a3c5cccc5427b1531e1cb
kigabu14 <i.m-sorry@hotmail.com>
2025-08-07 20:31:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,49 @@
 import streamlit as st
+from pprint import pprint
+from edgar.client import EdgarClient
+from dca.client import DCAClient
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Initialize the Edgar Client & DCA Client
+edgarClient = EdgarClient()
+dcaClient = DCAClient()
+
+# Setup tickers and period to fetch
+tickers = [
+    'CBOE',
+    'AAPL',
+    'WMT',
+    'F',
+    'HD',
+    'SONY',
+    'KO',
+    'GOOG',
+    'MCD',
+    'MCO',
+    'TSLA',
+    'MSFT',
+    'ZM',
+    'SNAP',
+    'DIS',
+    'NFLX',
+    'GPRO',
+    'BABA',
+    'CCL',
+    'PLTR',
+    'AMD',
+    'NVDA',
+    'SBUX',
+    'COIN', # no financial statements found..?
+    'T',
+]
+period = 'annual'
+
+# Loop through each ticker
+for ticker in tickers:
+    # Fetch financial information
+    financials = edgarClient.financials(ticker, period)
+    
+    # Run parameter using fetched financial information
+    dcaClient.runParameters(financials)
+
+# Finally write calculated parameters to an excel document
+dcaClient.writeToExcel()
