0101477c31f87c5e21e13432ca8ef49a0c174f90
kigabu14 <i.m-sorry@hotmail.com>
2025-08-12 16:27:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import io
 import datetime
 import numpy as np
 import matplotlib.pyplot as plt
- import google.generativeai as genai
+import google.generativeai as genai
 
 
 # ----------------- Helper Functions -----------------
